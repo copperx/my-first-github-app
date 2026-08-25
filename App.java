8b2ad04bde6cfc7887657a0f5341531d72925959
Rafael Escalante <copperx@users.noreply.github.com>
2026-08-25 14:51:38 +0000

    Add two horizontal lines

--- App.java
+++ App.java
@@ -2,6 +2,9 @@ public class App
 {
     public static void main(String[] args)
     {
+        System.out.println("------------------------------");
         System.out.println("Hello from Github Codespaces!");
+        System.out.println("------------------------------");
+
     }
 }
